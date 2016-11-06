4b0af39af025190956d8ad414fb9ba102cb4f5e9
maiha <maiha@wota.jp>
2016-11-07 04:47:22 +0900

    failed

---
+++ spec/example_spec.cr
@@ -0,0 +1,69 @@
+require "./spec_helper"
+require "xml"
+
+private def parse(html : String)
+  Try(XML::Node).try{ XML.parse_html(html) }
+end
+
+private def find(t_node, tag)
+  t_node.map{|n| n.xpath_node("//#{tag}") || raise "#{tag} not found" }
+end
+
+private def text(t_node)
+  t_node.map{|n| n.text || raise "text not found" }
+end
+
+private def pure(t_node) : Array(String)
+  array = [] of String
+  t_node.foreach do |n|
+    array << n
+  end
+  return array
+end
+
+private def find_text(html, tag)
+  node = parse(html)
+  span = find(node, tag)
+  text(span)
+end
+
+private def valid_html
+  <<-EOF
+    <div>
+      <span>foo</span>
+      <span>bar</span>
+    </div>
+    EOF
+end
+
+private def broken_html
+  <<-EOF
+    <div>
+      <sp
+    EOF
+end
+
+private def empty_text_html
+  <<-EOF
+    <div/>
+    EOF
+end
+
+describe "Example" do
+  it "parse span from valid html" do
+    text = find_text(valid_html, "span")
+    pure(text).should eq(["foo"])
+  end
+
+  it "parse span from broken html" do
+    text = find_text(broken_html, "span")
+    pure(text).should eq([] of String)
+    text.failed.value.message.should eq("span not found")
+  end
+
+  it "parse span from empty text html" do
+    text = find_text(empty_text_html, "span")
+    pure(text).should eq([] of String)
+    text.failed.value.message.should eq("span not found")
+  end
+end
--- src/try.cr
+++ src/try.cr
@@ -11,6 +11,8 @@ abstract class Try(T)
   abstract def failure? : Bool
   abstract def value : T
   abstract def get : T
+  abstract def failed : Try[Exception]
+  abstract def foreach(&block : T -> U) : Nil
   abstract def map(&block : T -> U) : Try(U)
   abstract def recover(&block : Exception -> U) : Try(U)
 end
@@ -33,6 +35,14 @@ class Success(T) < Try(T)
     @value
   end
   
+  def failed
+    Failure(Exception).new(Exception.new("can't cast #{@value.class} to Exception"))
+  end
+  
+  def foreach(&block : T -> U)
+    yield(@value)
+  end
+
   def map(&block : T -> U) : Success(U) | Failure(U)
     Try(U).try { yield(@value) }
   end
@@ -56,6 +66,14 @@ class Failure(T) < Try(T)
     true
   end
   
+  def failed
+    Failure(Exception).new(@value)
+  end
+
+  def foreach(&block : T -> U)
+    nil
+  end
+
   def map(&block : T -> U) : Success(U) | Failure(U)
     Failure(U).new( @value )
   end
